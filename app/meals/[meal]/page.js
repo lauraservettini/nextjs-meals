286d00abed9634a5846a2dfd3a6afeba222c1268
lauraservettini <lauraservettini87@gmail.com>
2024-01-29 14:33:54 +0100

    add meals, community, meal details page, meals db

--- app/meals/[meal]/page.js
+++ app/meals/[meal]/page.js
@@ -1,8 +1,38 @@
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
+
 export default function MealDetail({ params}) {
+    const meal = getMeal(params.meal);
+
+    if(!meal){
+      // se lo slug non esiste nel database  stoppa l'esecuzione della pagina e visualizza la notFound page
+      notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
     return (
-      <main>
-        <h1>MealDetail</h1>
-        <p>{params.meal}</p>
-      </main>
+      <>
+        <header className={classes.header}>
+          <div className={classes.image}>
+            <Image src={meal.image} alt={meal.title} fill/>
+          </div>
+          <div className={classes.headerText} >
+            <h1>{meal.title}</h1>
+            <p className={classes.creator}>
+              by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+            </p>
+            <p className={classes.summary}>
+            {meal.summary}
+            </p>
+          </div>
+        </header>
+        <main>
+          <p className={classes.instructions}
+            dangerouslySetInnerHTML={{ __html: meal.instructions}}
+          ></p>
+        </main>
+      </>
     );
   }
---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+'use client';
+
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div className='error'>
+      <h1>Not Found</h1>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  )
+}
