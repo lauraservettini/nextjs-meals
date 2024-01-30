c74e2426e6d84990f7aed0e4b90b4eb027bc9ddf
lauraservettini <lauraservettini87@gmail.com>
2024-01-30 11:08:11 +0100

    finished project and add metadata

--- app/meals/[meal]/page.js
+++ app/meals/[meal]/page.js
@@ -3,6 +3,17 @@ import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
 import { notFound } from 'next/navigation';
 
+export async function generateMetadata({params}){
+  const meal = getMeal(params.meal);
+
+  if(!meal){
+    notFound();
+  }
+  return{
+   title: meal.title,
+   description: meal.summary,
+ };
+} 
 export default function MealDetail({ params}) {
     const meal = getMeal(params.meal);
 
